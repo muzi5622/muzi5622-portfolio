4748e0765eefd05a9af92519aa3d4cd79ca122cd
Muzamil <muzi786muzi@gmail.com>
2025-02-27 01:17:21 +0500

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import App from "./App";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <BrowserRouter basename="/muzamil-portfolio"> {/* ✅ Add basename */}
+  <BrowserRouter basename="/"> {/* ✅ Add basename */}
     <App />
   </BrowserRouter>
 );
